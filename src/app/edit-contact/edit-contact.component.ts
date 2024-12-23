1edab43ee148a52ac4a10329c21c321055aa2d1d
Benjamin Pack <benlevipack@gmail.com>
2024-12-23 07:54:14 -0500

    Completed module 3 for adding Reactive forms to the contacts app.

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,19 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  contactForm = this.fb.nonNullable.group({
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: <Date | null> null,
+    favoritesRanking: <number | null> null,
+    phone: this.fb.nonNullable.group ({
+      phoneNumber: '',
+      phoneType: ''
+    }),
+    address: this.fb.nonNullable.group({
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
+    }),
+  });
+
+  constructor(private route: ActivatedRoute, 
+    private contactService:ContactsService, 
+    private router: Router,
+    private fb: FormBuilder) { }
+
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return
+
+    this.contactService.getContact(contactId).subscribe((contact) => {
+      if (!contact) return;
+      this.contactForm.setValue(contact);
+      
+    });
   }
 
   saveContact() {
-
+    console.log(this.contactForm.value);
+    this.contactService.saveContact(this.contactForm.getRawValue()).subscribe({
+      next: () => this.router.navigate(['/contacts'])
+    });
   }
 }
